7bba54963a5a94a2a65a8a12ef403c6f02d6706a
Shrikrishna Temkar <krishnatemkar2002@gmail.com>
2026-02-10 15:52:48 +0530

    Frontend-only app, remove backend fetch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import Home from "./components/Home";
-// import BookDetails from "./components/BookDetails";
-// import BookForm from "./components/BookForm";
-
-// export default function App() {
-//   const [books, setBooks] = useState([]);
-
-//   useEffect(() => {
-//     fetch("http://localhost:5000/books")
-//       .then((res) => res.json())
-//       .then((data) => setBooks(data))
-//       .catch((err) => console.error("API error:", err));
-//   }, []);
-
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home books={books} setBooks={setBooks} />} />
-//         <Route path="/book/:id" element={<BookDetails books={books} />} />
-//         <Route path="/add" element={<BookForm />} />
-//         <Route path="/edit/:id" element={<BookForm />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -37,8 +8,18 @@ import BookForm from "./components/BookForm";
 export default function App() {
   const [books, setBooks] = useState([]);
 
- const storedBooks = localStorage.getItem("books");
- if (storedBooks) setBooks(JSON.parse(storedBooks));
+  // Load books from localStorage once on mount
+  useEffect(() => {
+    const storedBooks = localStorage.getItem("books");
+    if (storedBooks) {
+      setBooks(JSON.parse(storedBooks));
+    }
+  }, []);
+
+  // Save books to localStorage whenever books state changes
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BrowserRouter>
