b04106234dd0169abe01c87bb43b25e7b33d591f
Shrikrishna Temkar <krishnatemkar2002@gmail.com>
2026-02-10 15:50:11 +0530

    Remove all backend calls, frontend-only

--- src/App.js
+++ src/App.js
@@ -37,12 +37,8 @@ import BookForm from "./components/BookForm";
 export default function App() {
   const [books, setBooks] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/books")
-      .then((res) => res.json())
-      .then((data) => setBooks(data))
-      .catch((err) => console.error("API error:", err));
-  }, []);
+ const storedBooks = localStorage.getItem("books");
+ if (storedBooks) setBooks(JSON.parse(storedBooks));
 
   return (
     <BrowserRouter>
